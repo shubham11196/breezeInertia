fe90fd1902625352e5c95849ffe96e29cabb6d6f
Shubham Bhatt <shubham@crackerdigital.com>
2022-03-28 14:01:14 +0530

    fixed some changes

--- resources/js/Layouts/TopbarComponent.vue
+++ resources/js/Layouts/TopbarComponent.vue
@@ -169,7 +169,7 @@
                     <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i>
                         Help Center</a>
                     <div class="dropdown-divider"></div> -->
-                    <button class="dropdown-item" type="button" @click="logOut" name="logout"> Log Out</button>
+                    <button class="dropdown-item" type="button" @click="logout" name="logout"> Log Out</button>
                 </div>
             </li>
         </ul>
